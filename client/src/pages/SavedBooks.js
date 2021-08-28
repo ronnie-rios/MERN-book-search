bce4fcbf072ff4e6cbee31543f044957be32da7f
Ronnie Rios <ronniesmail14@gmail.com>
2021-08-28 17:49:25 -0500

    fixed savedbook signup

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 import { useQuery, useMutation } from '@apollo/react-hooks';
 import { GET_ME } from '../utils/queries';
+import { REMOVE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
