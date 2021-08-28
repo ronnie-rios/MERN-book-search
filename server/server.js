615f410c47edcdb842a641852ae01e18eead7f9a
Ronnie Rios <ronniesmail14@gmail.com>
2021-08-28 16:32:41 -0500

    updated server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
-const express = require('express');
 const path = require('path');
+const express = require('express');
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
+const app = express();
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+async function startup() {
+  await server.start();
+  server.applyMiddleware({ app });
+}
+startup();
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
